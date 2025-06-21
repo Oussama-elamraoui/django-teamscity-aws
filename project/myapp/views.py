5680ba88aaf8dd256a7f6ff8a087b323684b2a64
OUSSAMA <oussama.elamraoui99@gmail.com>
2025-06-21 21:49:56 +0100

    firsst push

---
+++ project/myapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.views import View
+
+# Create your views here.
+class HelloWorldView(View):
+    def get(self, request):
+
+        host = request.get_host()
+        ip = host.split(':')[0] 
+        return JsonResponse({
+            'message': 'Yes, you connected successfully!',
+            'api': ip
+        })
